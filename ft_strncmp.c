318cdcc4d82a608d033dc9ef40ffca939ed941b7
Aroon Wiriyokhoon <thaiseamonster@gmail.com>
2026-09-07 21:15:43 +0700

    ft_strncmp.cCompleateRevise

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -23,12 +23,6 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
     {
         if(s1[i] != s2[i])
             return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-        if((s1[i] == '\0') && (s2[i] == '\0'))
-            return (0);
-        if(s1[i] == '\0')
-            return (48-s2[i]);
-        if(s2[i] == '\0')
-            return (s1[i] - 48);
         i++;
     }
     return (0);
